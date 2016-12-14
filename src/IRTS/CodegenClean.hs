eca03789cefa1f2f498d8ab4a006090ac9072401
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2016-12-14 11:03:14 +0100

    use pretty printer instead of string

--- src/IRTS/CodegenClean.hs
+++ src/IRTS/CodegenClean.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module IRTS.CodegenClean
     ( codegenClean
     ) where
+
+import Prelude hiding ((<$>))
 
 import IRTS.CodegenCommon
 import IRTS.Lang
@@ -8,91 +11,85 @@ import IRTS.Defunctionalise
 import Idris.Core.TT
 
 import Data.Char
-import Data.List
+import Data.Text.Lazy (pack, unpack)
+import System.IO
+import Text.PrettyPrint.Leijen.Text
 
 codegenClean :: CodeGenerator
 codegenClean info = do
-    let declarations = concatMap cgDecl (defunDecls info)
-    writeFile (outputFile info) $ unlines
-        [ header
-        , imports
-        , helpers
-        , declarations
-        , start
-        ]
+    let declarations = vsep $ map cgDecl (defunDecls info)
+    let output = vsep [header, imports, helpers, declarations, start]
+    withFile (outputFile info) WriteMode (`hPutDoc` output)
     where
         isFun (_, DFun{}) = True
         isFun (_, DConstructor{}) = False
 
-header, imports, helpers, start :: String
+header, imports, helpers, start :: Doc
 header = "module Main"
-imports = unlines
+imports = vsep
     [ "import StdEnv" ]
-helpers = unlines
+helpers = vsep
     [ ":: Value = .." ]
-start = unlines
+start = vsep
     [ "Start :: *World -> *World"
     , "Start world = runMain"
     ]
 
-cgDecl :: (Name, DDecl) -> String
+indentLevel :: Int
+indentLevel = 4
+
+cgDecl :: (Name, DDecl) -> Doc
 cgDecl (_, DConstructor name tag arity) = cgCtor name tag arity
 cgDecl (_, DFun name args def) = cgFun name args def
 
-cgCtor :: Name -> Int -> Int -> String
+cgCtor :: Name -> Int -> Int -> Doc
 cgCtor name tag arity =
     --FIXME strictness
-    ":: Value | " ++ cgName name ++ unwords (replicate arity "Value")
+    ":: Value |" <+> cgName name <+> hsep (replicate arity "Value")
 
-cgFun :: Name -> [Name] -> DExp -> String
+cgFun :: Name -> [Name] -> DExp -> Doc
 cgFun name args def =
-    "/// " ++ show name ++ "\n" ++
-    cgName name ++ " " ++ unwords (map (cgLoc . fst) (zip [0..] args)) ++ " = \n" ++ indent 1 (
-    cgExp def)
+    "///" <+> cgName name <$>
+    cgName name <+> hsep (map (cgLoc . fst) (zip [0..] args)) <+> char '=' <$>
+    indent indentLevel (cgExp def)
 
-cgExp :: DExp -> String
-cgExp exp = "(" ++ cgBody exp ++ ")"
-    where
-        cgBody :: DExp -> String
-        --FIXME parens!
-        cgBody (DV (Glob name)) =
-            cgName name
-        cgBody (DV (Loc idx)) =
-            cgLoc idx
-        cgBody (DApp _istailcall_ name args) =
-            cgApp name args
-        cgBody (DLet name def rest) =
-            --FIXME should be strict always?
-            "# " ++ cgName name ++ " = " ++ cgExp def ++ "\n" ++
-                cgExp rest ++ "\n"
-        cgBody (DUpdate var def) =
-            --FIXME what is this?
-            "UPDATE " ++ cgExp def ++ "\n"
-        cgBody (DProj def idx) =
-            cgExp def ++ "[" ++ show idx ++ "]"
-        cgBody (DC _reloc_ _type_ name args) =
-            cgApp name args
-        cgBody _ =
-            "NOT IMPLEMENTED"
+cgExp :: DExp -> Doc
+--FIXME parens!
+cgExp (DV (Glob name)) =
+    cgName name
+cgExp (DV (Loc idx)) =
+    cgLoc idx
+cgExp (DApp _istailcall_ name args) =
+    cgApp name args
+cgExp (DLet name def rest) =
+    --FIXME should be strict always?
+    char '#' <+> cgName name <+> char '=' <+> cgExp def <$>
+    cgExp rest <$> empty
+cgExp (DUpdate var def) =
+    --FIXME what is this?
+    "UPDATE " <+> cgExp def <$> empty
+cgExp (DProj def idx) =
+    cgExp def <+> brackets (int idx)
+cgExp (DC _reloc_ _type_ name args) =
+    cgApp name args
+cgExp _ =
+    "NOT IMPLEMENTED"
 
 ----
 
-cgApp :: Name -> [DExp] -> String
-cgApp name args = cgName name ++ " " ++ unwords (map cgExp args)
+cgApp :: Name -> [DExp] -> Doc
+cgApp name args = cgName name <+> hsep (map cgExp args)
 
-cgVar :: LVar -> String
+cgVar :: LVar -> Doc
 cgVar (Loc idx) = cgLoc idx
 cgVar (Glob name) = cgName name
 
-cgLoc :: Int -> String
-cgLoc idx = "loc" ++ show idx
+cgLoc :: Int -> Doc
+cgLoc idx = "loc" <> int idx
 
-cgName :: Name -> String
-cgName name = "idris_" ++ concatMap mangleChar (showCG name)
+cgName :: Name -> Doc
+cgName name = "idris_" <> (text . pack) (concatMap mangle $ showCG name)
     where
-        mangleChar c
+        mangle c
             | isAlpha c || isDigit c = [c]
             | otherwise = "_" ++ show (fromEnum c) ++ "_"
-
-indent :: Int -> String -> String
-indent n str = replicate (4 * n) ' ' ++ str
