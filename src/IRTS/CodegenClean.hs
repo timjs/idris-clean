c1b45ac4f35008e88fabc3833c10ea3ec404501b
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2016-12-15 15:51:40 +0100

    cleanup

--- src/IRTS/CodegenClean.hs
+++ src/IRTS/CodegenClean.hs
@@ -4,22 +4,23 @@ module IRTS.CodegenClean
     ) where
 
 import Prelude hiding ((<$>))
-import Numeric (showHex)
 
 import IRTS.CodegenCommon
 import IRTS.Lang
 import IRTS.Defunctionalise
 import Idris.Core.TT
 
+import Numeric (showHex)
 import Data.Char (isAlpha, isDigit, ord)
 import Data.Text.Lazy (Text, pack, unpack, toUpper)
+import System.IO (withFile, IOMode(..))
+
 import Text.PrettyPrint.Leijen.Text hiding (string)
-import System.IO
 
 -- Helpers ---------------------------------------------------------------------
 
-indentLevel :: Int
-indentLevel = 4
+indentlevel :: Int
+indentlevel = 4
 
 blank :: Doc
 blank = space
@@ -27,27 +28,47 @@ blank = space
 string :: String -> Doc
 string = text . pack
 
--- Code Generator --------------------------------------------------------------
+operator, call :: Text -> [Doc] -> Doc
+operator op [left, right] = parens $ left <+> text op <+> right
+call func args = parens $ text func <+> hsep args
+
+mangle :: Name -> Text
+mangle name = pack (concatMap mangleChar $ showCG name)
+    where
+        mangleChar c
+            | isIdent c = [c]
+            | isSep c = "_"
+            | isBrace c = ""
+            | otherwise = "_" ++ show (fromEnum c) ++ "_"
+        isIdent c = isAlpha c || isDigit c || c == '_'
+        isSep c = c == '.'
+        isBrace c = c == '{' || c == '}'
+
+-- Main and Prelude ------------------------------------------------------------
 
 codegenClean :: CodeGenerator
 codegenClean info = do
     let declarations = vsep $ map cgDecl (defunDecls info)
-    let output = vsep [header, imports, helpers, declarations, start]
+    let output = vsep [cgPrelude, declarations]
     withFile (outputFile info) WriteMode (`hPutDoc` output)
     where
         isFun (_, DFun{}) = True
         isFun (_, DConstructor{}) = False
 
-header, imports, helpers, start :: Doc
-header = "module Main"
-imports = vsep
-    [ "import StdEnv" ]
-helpers = vsep
-    [ ":: Value = .." ]
-start = vsep
-    [ "Start :: *World -> *World"
+cgPrelude :: Doc
+cgPrelude = vsep
+    [ "module Main"
+    , blank
+    , "import StdEnv"
+    , blank
+    , ":: Value = .."
+    , blank
+    , "Start :: *World -> *World"
     , "Start world = runMain"
+    , blank
     ]
+
+-- Declarations and Expressions ------------------------------------------------
 
 cgDecl :: (Name, DDecl) -> Doc
 cgDecl (_, DConstructor name tag arity) = cgCtor name tag arity
@@ -64,14 +85,11 @@ cgFun name args def =
     blank <$>
     "///" <+> string (show name) <$>
     cgName name <+> hsep (map cgName args) <+> char '=' <$>
-    indent indentLevel (cgExp def)
+    indent indentlevel (cgExp def)
 
 cgExp :: DExp -> Doc
---FIXME parens!
-cgExp (DV (Glob name)) =
-    cgName name
-cgExp (DV (Loc idx)) =
-    cgLoc idx
+cgExp (DV var) =
+    cgVar var
 cgExp (DApp _istail name args) =
     cgApp name args
 cgExp (DLet name def rest) =
@@ -80,8 +98,7 @@ cgExp (DLet name def rest) =
     cgExp rest <$>
     blank
 cgExp (DUpdate var def) =
-    --FIXME what is this?
-    "UPDATE " <+> cgExp def <$> empty
+    cgUnsupported "update" (var, def)
 cgExp (DProj def idx) =
     cgExp def <+> brackets (int idx)
 cgExp (DC _reloc _tag name args) =
@@ -98,7 +115,7 @@ cgExp (DOp prim exps) =
 cgExp DNothing =
     empty
 cgExp (DError msg) =
-    "error" <+> string msg
+    "abort" <+> dquotes (string msg)
 cgExp e =
     cgUnsupported "expression" e
 
@@ -116,22 +133,16 @@ cgAlt (DDefaultCase exp) =
     char '_' <+> "->" <+> cgExp exp
 
 cgApp :: Name -> [DExp] -> Doc
-cgApp name args = cgName name <+> hsep (map cgExp args)
+cgApp name args = call (mangle name) (map cgExp args)
 
 -- Constants and Primitives ----------------------------------------------------
 
 cgConst :: Const -> Doc
 cgConst (I i) = int i
 cgConst (Fl d) = double d
-cgConst (Ch c) = cgChar c
-cgConst (Str s) = cgString s
+cgConst (Ch c) = squotes . string . cgEscape False $ c
+cgConst (Str s) = dquotes . string . concatMap (cgEscape True) $ s
 cgConst c = cgUnsupported "constant" c
-
-cgChar :: Char -> Doc
-cgChar = squotes . string . cgEscape False
-
-cgString :: String -> Doc
-cgString = dquotes . string . concatMap (cgEscape True)
 
 cgEscape :: Bool -> Char -> String
 cgEscape True '"' = "\\\""
@@ -142,58 +153,54 @@ cgEscape isString c
     | c <= '\xFF' = "\\x" ++ showHex (ord c) ""
     | otherwise = error $ "idris-codegen-clean: char " ++ show c ++ " is bigger than 255"
 
-cgInfix, cgPrefix :: Text -> [Doc] -> Doc
-cgInfix op [left, right] = parens $ left <+> text op <+> right
-cgPrefix op args = parens $ text op <+> hsep args
-
 cgPrim :: PrimFn -> [Doc] -> Doc
-cgPrim (LPlus _) = cgInfix "+"
-cgPrim (LMinus _) = cgInfix "-"
-cgPrim (LTimes _) = cgInfix "*"
-cgPrim (LUDiv _) = cgInfix "/"
-cgPrim (LSDiv _) = cgInfix "/"
-cgPrim (LURem _) = cgInfix "rem"
-cgPrim (LSRem _) = cgInfix "rem"
+cgPrim (LPlus _) = operator "+"
+cgPrim (LMinus _) = operator "-"
+cgPrim (LTimes _) = operator "*"
+cgPrim (LUDiv _) = operator "/"
+cgPrim (LSDiv _) = operator "/"
+cgPrim (LURem _) = operator "rem"
+cgPrim (LSRem _) = operator "rem"
 
-cgPrim (LAnd _) = cgInfix "bitand"
-cgPrim (LOr _) = cgInfix "bitor"
-cgPrim (LXOr _) = cgInfix "bitxor"
-cgPrim (LSHL _) = cgInfix "<<"
-cgPrim (LASHR _) = cgInfix ">>"
-cgPrim (LLSHR _) = cgInfix ">>"  --FIXME
+cgPrim (LAnd _) = operator "bitand"
+cgPrim (LOr _) = operator "bitor"
+cgPrim (LXOr _) = operator "bitxor"
+cgPrim (LSHL _) = operator "<<"
+cgPrim (LASHR _) = operator ">>"
+cgPrim (LLSHR _) = operator ">>"  --FIXME
 --cgPrim (LCompl _) = \[x] -> text "~" <> x
 
-cgPrim (LEq _) = cgInfix "=="
-cgPrim (LLt _) = cgInfix "<"
-cgPrim (LSLt _) = cgInfix "<"
-cgPrim (LLe _) = cgInfix "<="
-cgPrim (LSLe _) = cgInfix "<="
-cgPrim (LGt _) = cgInfix ">"
-cgPrim (LSGt _) = cgInfix ">"
-cgPrim (LGe _) = cgInfix ">="
-cgPrim (LSGe _) = cgInfix ">="
+cgPrim (LEq _) = operator "=="
+cgPrim (LLt _) = operator "<"
+cgPrim (LSLt _) = operator "<"
+cgPrim (LLe _) = operator "<="
+cgPrim (LSLe _) = operator "<="
+cgPrim (LGt _) = operator ">"
+cgPrim (LSGt _) = operator ">"
+cgPrim (LGe _) = operator ">="
+cgPrim (LSGe _) = operator ">="
 
 -- cgPrim (LSExt _ _) = head
 -- cgPrim (LZExt _ _) = head
 -- cgPrim (LTrunc _ _) = head
 -- cgPrim (LBitCast _ _) = head
 
-cgPrim (LChInt _) = cgPrefix "toInt"
-cgPrim (LIntCh _) = cgPrefix "fromInt"
+cgPrim (LChInt _) = call "toInt"
+cgPrim (LIntCh _) = call "fromInt"
 
-cgPrim (LIntStr _) = cgPrefix "toString"
-cgPrim (LStrInt _) = cgPrefix "fromString"
-cgPrim LStrConcat = cgInfix "+++"
-cgPrim LStrLt = cgInfix "<"
-cgPrim LStrEq = cgInfix "=="
+cgPrim (LIntStr _) = call "toString"
+cgPrim (LStrInt _) = call "fromString"
+cgPrim LStrConcat = operator "+++"
+cgPrim LStrLt = operator "<"
+cgPrim LStrEq = operator "=="
 
---cgPrim  LStrRev    = cgPrefix "reverse"
---cgPrim  LStrCons   = cgPrefix "cons"
+--cgPrim  LStrRev    = call "reverse"
+--cgPrim  LStrCons   = call "cons"
 --cgPrim  LStrHead   = \[x] -> x ! "0"
 --cgPrim  LStrTail   = \[x] -> x ! "1:"
 --cgPrim  LStrIndex  = \[x,i] -> x <> brackets i
 --cgPrim  LStrLen    = cgPFun "len"
---cgPrim LStrSubstr = \[ofs,len,s] -> s <> brackets (ofs <> colon <> cgInfix "+" [ofs,len])
+--cgPrim LStrSubstr = \[ofs,len,s] -> s <> brackets (ofs <> colon <> operator "+" [ofs,len])
 
 cgPrim LWriteStr = \[_world, str] ->
     "fwrites" <+> str <+> "stdio" <$>
@@ -203,23 +210,23 @@ cgPrim LReadStr = \[_world] ->
 
 --cgPrim (LExternal n) = cgExtern $ show n
 
-cgPrim (LIntFloat _) = cgPrefix "toReal"
-cgPrim (LFloatInt _) = cgPrefix "fromReal"
-cgPrim LFloatStr = cgPrefix "toString"
-cgPrim LStrFloat = cgPrefix "fromString"
+cgPrim (LIntFloat _) = call "toReal"
+cgPrim (LFloatInt _) = call "fromReal"
+cgPrim LFloatStr = call "toString"
+cgPrim LStrFloat = call "fromString"
 
-cgPrim LFExp = cgPrefix "exp"
-cgPrim LFLog = cgPrefix "log"
-cgPrim LFSin = cgPrefix "sin"
-cgPrim LFCos = cgPrefix "cos"
-cgPrim LFTan = cgPrefix "tan"
-cgPrim LFASin = cgPrefix "asin"
-cgPrim LFACos = cgPrefix "acos"
-cgPrim LFATan = cgPrefix "atan"
-cgPrim LFSqrt = cgPrefix "sqrt"
-cgPrim LFFloor = cgPrefix "floor"
-cgPrim LFCeil  = cgPrefix "ceil"
-cgPrim LFNegate = cgPrefix "~" -- \[x] -> text "~" <> x
+cgPrim LFExp = call "exp"
+cgPrim LFLog = call "log"
+cgPrim LFSin = call "sin"
+cgPrim LFCos = call "cos"
+cgPrim LFTan = call "tan"
+cgPrim LFASin = call "asin"
+cgPrim LFACos = call "acos"
+cgPrim LFATan = call "atan"
+cgPrim LFSqrt = call "sqrt"
+cgPrim LFFloor = call "floor"
+cgPrim LFCeil  = call "ceil"
+cgPrim LFNegate = call "~" -- \[x] -> text "~" <> x
 
 cgPrim f = \_args -> cgUnsupported "primitive" f
 
@@ -233,17 +240,10 @@ cgLoc :: Int -> Doc
 cgLoc idx = "loc" <> int idx
 
 cgName :: Name -> Doc
-cgName name = string (concatMap mangle $ showCG name)
-    where
-        mangle c
-            | isIdent c = [c]
-            | isSep c = "_"
-            | isBrace c = ""
-            | otherwise = "_" ++ show (fromEnum c) ++ "_"
-        isIdent c = isAlpha c || isDigit c || c == '_'
-        isSep c = c == '.'
-        isBrace c = c == '{' || c == '}'
+cgName = text . mangle
+
+-- Unsupported -----------------------------------------------------------------
 
 cgUnsupported :: Show a => Text -> a -> Doc
 cgUnsupported cat val =
-    "abort" <+> dquotes ("UNSUPPORTED" <+> text (toUpper cat) <+> string (show val))
+    call "abort" [dquotes $ "UNSUPPORTED" <+> text (toUpper cat) <+> string (show val)]
