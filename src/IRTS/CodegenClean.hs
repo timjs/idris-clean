d55a601521db1c502e627637b072158e3b0e2671
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2016-12-19 18:56:25 +0100

    add boxing, fix indent

--- src/IRTS/CodegenClean.hs
+++ src/IRTS/CodegenClean.hs
@@ -17,21 +17,28 @@ import Data.Text.Lazy (Text, pack, unpack)
 import System.IO
 import System.FilePath
 
-import Text.PrettyPrint.Leijen.Text hiding (string)
+import Text.PrettyPrint.Leijen.Text hiding (indent, string)
+import qualified Text.PrettyPrint.Leijen.Text as Pretty
 
 -- Helpers ---------------------------------------------------------------------
 
-level :: Int
-level = 4
-
-blank :: Doc
-blank = space
+indent :: Doc -> Doc
+indent = Pretty.indent 4
 
 string :: String -> Doc
 string = text . pack
 
+blank :: Doc
+blank = space
+
 deline :: String -> String
 deline = map (\c -> if c == '\n' then ' ' else c)
+
+prefixf, infixf :: Doc -> [Doc] -> Doc
+prefixf fun args = parens $
+    fun <+> hsep args
+infixf op [left, right] = parens $
+    left <+> op <+> right
 
 -- Main and Prelude ------------------------------------------------------------
 
@@ -41,7 +48,7 @@ codegenClean info = do
     let output = vsep $ intersperse blank
             [ cgModule (takeBaseName $ outputFile info)
             , cgImports
-            , cgHelpers
+            , cgPredefined
             , cgConstructors ctors
             , cgFunctions funcs
             , cgStart
@@ -54,33 +61,42 @@ codegenClean info = do
 cgModule :: String -> Doc
 cgModule name = "module" <+> string name
 
-cgImports, cgHelpers, cgStart :: Doc
+cgImports, cgPredefined, cgStart :: Doc
 cgImports = vsep $ map ("import" <+>)
     [ "StdEnv" ]
-cgHelpers = vsep
-    [ "unsafeCoerce :: a -> b"
-    , "unsafeCoerce x = code inline {"
-    , indent level "pop_a 0"
-    , "}"
+cgPredefined = vsep
+    [ ":: Value = Dummy"
+    , indent "| Boxed_Bool !Bool"
+    , indent "| Boxed_Char !Char"
+    , indent "| Boxed_Int !Int"
+    , indent "| Boxed_Real !Real"
+    , indent "| Boxed_String !String"
+    , indent "| .."
+    , blank
+    , "unbox_Bool (Boxed_Bool x) :== x"
+    , "unbox_Char (Boxed_Char x) :== x"
+    , "unbox_Int (Boxed_Int x) :== x"
+    , "unbox_Real (Boxed_Real x) :== x"
+    , "unbox_String (Boxed_String x) :== x"
     ]
 cgStart = vsep
     [ "Start :: *World -> *World"
     , "Start world ="
-    , indent level ("let res =" <+> cgFunName (MN 0 "runMain") <+> "in" <+> "world")
+    , indent ("let res =" <+> cgFunName (MN 0 "runMain") <+> "in" <+> "world")
     ]
 
 -- Declarations and Expressions ------------------------------------------------
 
 cgConstructors :: [(Name, DDecl)] -> Doc
 cgConstructors decls =
-    ":: Idris_Value = Idris_Dummy_Value" <$>
-    indent level (vsep $ map (cgCon . snd) decls)
+    ":: Value" <$>
+    indent (vsep $ map (cgCon . snd) decls)
 
 cgCon :: DDecl -> Doc
 cgCon (DConstructor name tag arity) =
     --FIXME strictness
     "///" <+> string (show name) <+> parens (int tag) <$>
-    char '|' <+> cgConName name <+> hsep (replicate arity "Idris_Value")
+    char '|' <+> cgConName name <+> hsep (replicate arity "Value")
 
 cgFunctions :: [(Name, DDecl)] -> Doc
 cgFunctions = vsep . map (cgFun . snd)
@@ -91,7 +107,7 @@ cgFun (DFun name args def) =
     "///" <+> (string . show) name <$>
     "///" <+> (string . deline . show) def <$>
     cgFunName name <+> hsep (map cgVarName args) <+> char '=' <$>
-    indent level (cgExp def)
+    indent (cgExp def)
 
 cgExp :: DExp -> Doc
 cgExp (DV var) =
@@ -101,7 +117,7 @@ cgExp (DApp _istail name args) =
 cgExp (DLet name def rest) =
     --FIXME should be strict always?
     "let" <+> cgVarName name <+> char '=' <+> cgExp def <+> "in" <$>
-    indent level (cgExp rest) <$>
+    indent (cgExp rest) <$>
     blank
 cgExp (DUpdate var def) =
     cgUnsupported "UPDATE" (var, def)
@@ -109,9 +125,9 @@ cgExp (DProj def idx) =
     cgExp def <+> brackets (int idx)
 -- Constructors: False, True
 cgExp (DC _ 0 name []) | name == falseName =
-    "False"
+    cgBox BBool "False"
 cgExp (DC _ 1 name []) | name == trueName =
-    "True"
+    cgBox BBool "True"
 -- Constructors: rest
 cgExp (DC _reloc _tag name args) =
     --FIXME optimize to Int for argless ctors
@@ -147,16 +163,16 @@ cgExp e =
 cgIfThenElse :: DExp -> DExp -> DExp -> Doc
 cgIfThenElse test thenAlt elseAlt =
     "if" <+> parens (cgExp test) <$>
-    indent level (
+    indent (
         parens (cgExp thenAlt) <$>
-        parens(cgExp elseAlt)
+        parens (cgExp elseAlt)
     )
 
 cgCase :: DExp -> [DAlt] -> Doc
 cgCase exp alts =
     -- parens for `case` in `case`
     "case" <+> parens (cgExp exp) <+> "of" <$>
-    indent level (vsep (map cgAlt alts))
+    indent (vsep (map cgAlt alts))
 
 cgAlt :: DAlt -> Doc
 cgAlt (DConCase _tag name args exp) =
@@ -169,12 +185,12 @@ cgAlt (DDefaultCase exp) =
 -- Constants and Primitives ----------------------------------------------------
 
 cgConst :: Const -> Doc
-cgConst (I i) = int i
+cgConst (I i) = cgBox BInt $ int i
 --FIXME to big...
-cgConst (BI i) = integer i
-cgConst (Fl d) = double d
-cgConst (Ch c) = squotes . string . cgEscape False $ c
-cgConst (Str s) = dquotes . string . concatMap (cgEscape True) $ s
+cgConst (BI i) = cgBox BInt $ integer i
+cgConst (Fl d) = cgBox BReal $ double d
+cgConst (Ch c) = cgBox BChar $ squotes . string . cgEscape False $ c
+cgConst (Str s) = cgBox BString $ dquotes . string . concatMap (cgEscape True) $ s
 cgConst c = cgUnsupported "CONSTANT" c
 
 cgEscape :: Bool -> Char -> String
@@ -187,45 +203,40 @@ cgEscape isString c
     | otherwise = error $ "idris-codegen-clean: char " ++ show c ++ " is bigger than 255"
 
 cgPrim :: PrimFn -> [DExp] -> Doc
-cgPrim (LPlus _) = cgOp "+"
-cgPrim (LMinus _) = cgOp "-"
-cgPrim (LTimes _) = cgOp "*"
-cgPrim (LUDiv _) = cgOp "/"
-cgPrim (LSDiv _) = cgOp "/"
-cgPrim (LURem _) = cgOp "rem"
-cgPrim (LSRem _) = cgOp "rem"
+cgPrim (LPlus  ty) = cgDeboxOp (cgATy ty) "+"
+cgPrim (LMinus ty) = cgDeboxOp (cgATy ty) "-"
+cgPrim (LTimes ty) = cgDeboxOp (cgATy ty) "*"
+cgPrim (LSDiv  ty) = cgDeboxOp (cgATy ty) "/"
+cgPrim (LUDiv  ty) = cgDeboxOp (cgITy ty) "/"
+cgPrim (LSRem  ty) = cgDeboxOp (cgATy ty) "rem"
+cgPrim (LURem  ty) = cgDeboxOp (cgITy ty) "rem"
 
-cgPrim (LAnd _) = cgOp "bitand"
-cgPrim (LOr _) = cgOp "bitor"
-cgPrim (LXOr _) = cgOp "bitxor"
-cgPrim (LSHL _) = cgOp "<<"
-cgPrim (LASHR _) = cgOp ">>"
-cgPrim (LLSHR _) = cgOp ">>"  --FIXME
+cgPrim (LAnd   ty) = cgDeboxOp (cgITy ty) "bitand"
+cgPrim (LOr    ty) = cgDeboxOp (cgITy ty) "bitor"
+cgPrim (LXOr   ty) = cgDeboxOp (cgITy ty) "bitxor"
+cgPrim (LSHL   ty) = cgDeboxOp (cgITy ty) "<<"
+cgPrim (LASHR  ty) = cgDeboxOp (cgITy ty) ">>"
+cgPrim (LLSHR  ty) = cgDeboxOp (cgITy ty) ">>"  --FIXME
 --cgPrim (LCompl _) = \[x] -> text "~" <> x
 
-cgPrim (LEq _) = cgOp "=="
-cgPrim (LLt _) = cgOp "<"
-cgPrim (LSLt _) = cgOp "<"
-cgPrim (LLe _) = cgOp "<="
-cgPrim (LSLe _) = cgOp "<="
-cgPrim (LGt _) = cgOp ">"
-cgPrim (LSGt _) = cgOp ">"
-cgPrim (LGe _) = cgOp ">="
-cgPrim (LSGe _) = cgOp ">="
+cgPrim (LEq    ty) = cgReboxOp (cgATy ty) BBool "=="
+cgPrim (LLt    ty) = cgReboxOp (cgITy ty) BBool "<"
+cgPrim (LSLt   ty) = cgReboxOp (cgATy ty) BBool "<"
+cgPrim (LLe    ty) = cgReboxOp (cgITy ty) BBool "<="
+cgPrim (LSLe   ty) = cgReboxOp (cgATy ty) BBool "<="
+cgPrim (LGt    ty) = cgReboxOp (cgITy ty) BBool ">"
+cgPrim (LSGt   ty) = cgReboxOp (cgATy ty) BBool ">"
+cgPrim (LGe    ty) = cgReboxOp (cgITy ty) BBool ">="
+cgPrim (LSGe   ty) = cgReboxOp (cgATy ty) BBool ">="
 
 -- cgPrim (LSExt _ _) = head
 -- cgPrim (LZExt _ _) = head
 -- cgPrim (LTrunc _ _) = head
 -- cgPrim (LBitCast _ _) = head
 
-cgPrim (LChInt _) = cgApp "toInt"
-cgPrim (LIntCh _) = cgApp "fromInt"
-
-cgPrim (LIntStr _) = cgApp "toString"
-cgPrim (LStrInt _) = cgApp "fromString"
-cgPrim LStrConcat = cgOp "+++"
-cgPrim LStrLt = cgOp "<"
-cgPrim LStrEq = cgOp "=="
+cgPrim LStrConcat  = cgDeboxOp BString "+++"
+cgPrim LStrLt      = cgReboxOp BString BBool "<"
+cgPrim LStrEq      = cgReboxOp BString BBool "=="
 
 --cgPrim  LStrRev    = cgApp "reverse"
 --cgPrim  LStrCons   = cgApp "cons"
@@ -243,31 +254,76 @@ cgPrim LReadStr = \[_world] ->
 
 --cgPrim (LExternal n) = cgExtern $ show n
 
-cgPrim (LIntFloat _) = cgApp "toReal"
-cgPrim (LFloatInt _) = cgApp "fromReal"
-cgPrim LFloatStr = cgApp "toString"
-cgPrim LStrFloat = cgApp "fromString"
+cgPrim (LChInt ty)    = cgReboxApp BChar (cgITy ty) "toInt"
+cgPrim (LIntCh ty)    = cgReboxApp (cgITy ty) BChar "fromInt"
+cgPrim (LIntStr ty)   = cgReboxApp (cgITy ty) BString "toString"
+cgPrim (LStrInt ty)   = cgReboxApp BString (cgITy ty) "fromString"
+cgPrim (LIntFloat ty) = cgReboxApp (cgITy ty) BReal "toReal"
+cgPrim (LFloatInt ty) = cgReboxApp BReal (cgITy ty) "fromReal"
+cgPrim LFloatStr      = cgReboxApp BReal BString "toString"
+cgPrim LStrFloat      = cgReboxApp BString BReal "fromString"
 
-cgPrim LFExp = cgApp "exp"
-cgPrim LFLog = cgApp "log"
-cgPrim LFSin = cgApp "sin"
-cgPrim LFCos = cgApp "cos"
-cgPrim LFTan = cgApp "tan"
-cgPrim LFASin = cgApp "asin"
-cgPrim LFACos = cgApp "acos"
-cgPrim LFATan = cgApp "atan"
-cgPrim LFSqrt = cgApp "sqrt"
-cgPrim LFFloor = cgApp "floor"
-cgPrim LFCeil  = cgApp "ceil"
-cgPrim LFNegate = cgApp "~" -- \[x] -> text "~" <> x
+cgPrim LFExp    = cgDeboxApp BReal "exp"
+cgPrim LFLog    = cgDeboxApp BReal "log"
+cgPrim LFSin    = cgDeboxApp BReal "sin"
+cgPrim LFCos    = cgDeboxApp BReal "cos"
+cgPrim LFTan    = cgDeboxApp BReal "tan"
+cgPrim LFASin   = cgDeboxApp BReal "asin"
+cgPrim LFACos   = cgDeboxApp BReal "acos"
+cgPrim LFATan   = cgDeboxApp BReal "atan"
+cgPrim LFSqrt   = cgDeboxApp BReal "sqrt"
+cgPrim LFFloor  = cgDeboxApp BReal "floor"
+cgPrim LFCeil   = cgDeboxApp BReal "ceil"
+cgPrim LFNegate = cgDeboxApp BReal "~" -- \[x] -> text "~" <> x
 
 cgPrim f = \_args -> cgUnsupported "PRIMITIVE" f
 
--- Names -----------------------------------------------------------------------
+cgDeboxApp, cgDeboxOp :: BoxedTy -> Doc -> [DExp] -> Doc
+cgDeboxApp ty = cgReboxApp ty ty
+cgDeboxOp ty = cgReboxOp ty ty
+cgReboxApp, cgReboxOp :: BoxedTy -> BoxedTy -> Doc -> [DExp] -> Doc
+cgReboxApp = cgRebox prefixf
+cgReboxOp = cgRebox infixf
+
+cgRebox :: (Doc -> [Doc] -> Doc) -> BoxedTy -> BoxedTy -> Doc -> [DExp] -> Doc
+cgRebox app from to fun = cgBox to . app fun . map (cgUnbox from . cgExp)
+
+data BoxedTy
+    = BBool
+    | BChar
+    | BInt
+    | BReal
+    | BString
+
+instance Pretty BoxedTy where
+    pretty BBool = "Bool"
+    pretty BChar = "Char"
+    pretty BInt = "Int"
+    pretty BReal = "Real"
+    pretty BString = "String"
+
+cgITy :: IntTy -> BoxedTy
+cgITy ITNative = BInt
+cgITy ITBig = BInt
+cgITy ITChar = BChar
+cgITy (ITFixed IT8) = BChar
+cgITy (ITFixed IT16) = BInt
+cgITy (ITFixed IT32) = BInt
+cgITy (ITFixed IT64) = BInt
+
+cgATy :: ArithTy -> BoxedTy
+cgATy ATFloat = BReal
+cgATy (ATInt ity) = cgITy ity
+
+-- Names & Applications --------------------------------------------------------
+
+cgBox, cgUnbox :: BoxedTy -> Doc -> Doc
+cgBox ty exp = prefixf ("Boxed_" <> pretty ty) [exp]
+cgUnbox ty exp = prefixf ("unbox_" <> pretty ty) [exp]
 
 cgApp, cgOp :: Doc -> [DExp] -> Doc
-cgApp fun args = parens $ fun <+> hsep (map cgExp args)
-cgOp op [left, right] = parens $ cgExp left <+> op <+> cgExp right
+cgOp op [left, right] = infixf op [cgExp left, cgExp right]
+cgApp fun args = prefixf fun (map cgExp args)
 
 cgVar :: LVar -> Doc
 cgVar (Loc idx) = cgLoc idx --FIXME not in ir?
